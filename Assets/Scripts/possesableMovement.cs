325b89d541f291e4af0228948a8dd39cec1c82ad
Gonzako <al341522@uji.es>
2020-01-03 21:37:06 +0100

    added coyote time + tidied up script

--- Assets/Scripts/possesableMovement.cs
+++ Assets/Scripts/possesableMovement.cs
@@ -6,7 +6,9 @@
  */
 
 using System;
+using System.Collections;
 using UnityEngine;
+
 
 public class possesableMovement : MonoBehaviour, IPossesable
 {
@@ -16,11 +18,21 @@ public class possesableMovement : MonoBehaviour, IPossesable
     #endregion
 
     #region Private Fields
+    [SerializeField, Range(0,1)]
+    float contactLeeway;
+    [SerializeField]
     bool possessed = false;
+    [SerializeField, Range(0, 1)]
+    float coyoteTime = 0.3f;
     Rigidbody2D rb;
+    Collider2D col;
     [SerializeField]
     float jumpForce, horizontalSpeed;
+    [SerializeField]
+    float raycastDownLeeway = 0.2f;
     int horizontalInput = 0;
+    bool jump = false, grounded = false;
+    Coroutine groundedCoroutine;
     #endregion
 
     #region Public Methods
@@ -33,6 +45,12 @@ public class possesableMovement : MonoBehaviour, IPossesable
     #endregion
 
     #region Private Methods
+    private IEnumerator stopGrounded()
+    {
+        yield return new WaitForSeconds(coyoteTime);
+        grounded = false;
+        groundedCoroutine = null;
+    }
     #endregion
 
 
@@ -42,19 +60,59 @@ public class possesableMovement : MonoBehaviour, IPossesable
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
+        col = GetComponent<Collider2D>();
     }
  
     void Update()
     {
-        horizontalInput = (int)Input.GetAxisRaw("Horizontal");
+        if (possessed)
+        {
+            horizontalInput = (int)Input.GetAxisRaw("Horizontal");
+            jump = Input.GetButton("Jump") && grounded;
+
+        }
     }
 
     void FixedUpdate()
     {
+        if (possessed)
+        {
 
+            rb.AddForce(Vector2.right * horizontalInput * horizontalSpeed * Time.fixedDeltaTime);
+            horizontalInput = 0;
+            if (jump)
+            {
+                rb.AddForce(Vector2.up * jumpForce);
+                grounded = false;
+                jump = false;
+            }
+        }
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (possessed)
+        {
+            if (collision.contactCount == 0)
+            {
+                if(groundedCoroutine == null && grounded)
+                {
+                    groundedCoroutine = StartCoroutine("stopGrounded");
+                        
+                }
+            }
+            else
+            {
+                for (int i = 0; i < collision.contactCount; i++)
+                {
+                    if (Vector2.Dot(collision.GetContact(i).normal.normalized, Vector2.up) >= contactLeeway)
+                    {
+                        grounded = true;
+                    }
+                }
+            }
+        }
+    }
     #endregion
 #endif
 
