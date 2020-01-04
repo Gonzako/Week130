2d978f86fae3ba5043a9738b0553f8ed81242de0
Daryoush Farsimadan <farsimadan.daryoush@gmail.com>
2020-01-04 11:32:06 +0200

    GoalHandler done. Starting work on GameManager

---
+++ Assets/Scripts/GoalHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalHandler : MonoBehaviour
+{
+    public delegate void GoalEvents();
+    public GoalEvents onPlayerTouched;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            onPlayerTouched?.Invoke();
+        }
+    }
+}
