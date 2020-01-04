9518dbb6e6e37828b7de1031c8e1a5dfe3eb1cba
Gonzako <al341522@uji.es>
2020-01-04 18:37:03 +0100

    added layer stuff

---
+++ Assets/Scripts/possesableAnimationHAndler.cs
@@ -0,0 +1,39 @@
+﻿/*
+ *
+ * Copyright (c) Gonzako
+ * Gonzako123@gmail.com
+ *
+ */
+ 
+using UnityEngine;
+ 
+public class possesableAnimationHAndler : MonoBehaviour
+{
+    #region Public Fields
+    #endregion
+ 
+    #region Private Fields
+    #endregion
+
+    #region Public Methods
+    #endregion
+
+    #region Private Methods
+    #endregion
+
+
+    #if false
+    #region Unity API
+
+    void Start()
+    {
+    }
+ 
+    void FixedUpdate()
+    {
+    }
+
+    #endregion
+    #endif
+ 
+}
