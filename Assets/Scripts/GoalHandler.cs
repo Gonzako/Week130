ea4d342947dcfcc632b496404de60f5f28f59a7d
Gonzako <al341522@uji.es>
2020-01-07 13:44:21 +0100

    Update GoalHandler.cs

--- Assets/Scripts/GoalHandler.cs
+++ Assets/Scripts/GoalHandler.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class GoalHandler : MonoBehaviour
 {
+    
     public delegate void GoalEvents();
     public static GoalEvents onPlayerTouchedAnyGoal;
+    private const string playerPossesableTag = "PlayerControllable";
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Player")
+        if(collision.tag == playerPossesableTag)
         {
             Debug.Log("Goal Reached");
            onPlayerTouchedAnyGoal.Invoke();
