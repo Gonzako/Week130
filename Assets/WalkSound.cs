f3d623938657304667e42ad0e38524b3bc00946d
Eric Pratt <ericprattusc@gmail.com>
2020-01-06 23:43:41 -0800

    added footstep functionality off animation event/randomized and shuffled

--- Assets/WalkSound.cs
+++ Assets/WalkSound.cs
@@ -4,21 +4,61 @@ using UnityEngine;
 
 public class WalkSound : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private AudioClip[] walkSounds;
+
+    public static int shuffle;
+   
+    //Target shuffle length. Need a non static variable to show in Unity inspector. 
+    public int setShuffle;
+
+    //Recently played sound index
+    public int[] index;
+    private int currentIndex = 0;
+
+    AudioSource audioSource; 
     void Start()
     {
 
+        audioSource = GetComponent<AudioSource>();
+        shuffle = setShuffle;
+        //initializing index length to the same as shuffle
+        index = new int[shuffle];
+
+        //Setting all values in index to -1 at start. Done as a placeholder for an array value we will never have. 
+        for (int i = 0; i < shuffle; i++) { index[i] = -1; }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 
 
     void playWalk()
     {
-        Debug.Log("POOP"); 
+        int soundPlaying;
+        //The while loop cycles through the index checking for a previously played sound. If it finds one while loop starts again picking another random value. If not breaks from loop. 
+        while (true)
+        {
+            soundPlaying = randomSound();
+            bool foundSoundPlaying = false;
+            for (int i = 0; i < shuffle; i++)
+            {
+                if (index[i] == soundPlaying) { foundSoundPlaying = true; }
+            }
+            if (!foundSoundPlaying) { break; }
+        }
+        index[currentIndex] = soundPlaying;
+        Debug.Log(index[currentIndex]);
+        currentIndex++;
+        //Plays sound. Apparently this method is better as we made a variable and stored it rather than using a getter each time. 
+        audioSource.clip = walkSounds[soundPlaying];
+        audioSource.Play();
+        //Resets currentIndex when it equals shuffle number
+    }
+
+
+    int randomSound()
+    {
+
+        int random = Random.Range(0, walkSounds.Length);
+    return random; 
     }
 }
