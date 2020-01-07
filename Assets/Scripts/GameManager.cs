cb81ad133245a31fa46fadeba8b0de46b3e9ce7c
Gonzako <al341522@uji.es>
2020-01-07 13:09:33 +0100

    made failure state on character's body death instead of spike death
    Changed player tag to PlayerControllable

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,37 +10,41 @@ public class GameManager : MonoBehaviour
     public static GameEvents onLevelFail;
     public static GameEvents onLevelStart;
 
-    private GhostController _ghost;
+    private const string _playerBodyTag = "PlayerControllable";
 
+    private GhostController _ghost;
+    private killable _playerKillable;
 
     // Start is called before the first frame update
     private void Start()
     {
+
         onLevelStart?.Invoke();
     }
 
     private void CacheRefences()
     {
+        _playerKillable = GameObject.FindGameObjectWithTag(_playerBodyTag).GetComponent<killable>();
         _ghost = FindObjectOfType<GhostController>();
     }
 
     private void OnEnable()
     {
         CacheRefences();
-        spikeScript.onAnySpike += LevelFailure;
+        _playerKillable.onThisKill += LevelFailure;
         GoalHandler.onPlayerTouchedAnyGoal += LevelComplete;
     }
 
     private void OnDisable()
     {
-        spikeScript.onAnySpike -= LevelFailure;
+        _playerKillable.onThisKill += LevelFailure;
         GoalHandler.onPlayerTouchedAnyGoal -= LevelComplete;
     }
 
-    public void LevelFailure(GameObject ob, Collider2D col)
+    public void LevelFailure(GameObject caller)
     {
-        Debug.Log("Tag : "+ col.transform.tag);
-        if(col.transform.tag == "Player")
+        Debug.Log("Tag : "+ caller.transform.tag);
+        if(caller.transform.tag == "PlayerControllable")
             onLevelFail?.Invoke();
     }
 
